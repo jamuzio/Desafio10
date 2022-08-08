035125c9874537485314dfdb422ca3e128a8b65c
jamuzio <jamuzio@gmail.com>
2022-08-08 18:50:29 -0300

    Add PORT Env variable

--- main.js
+++ main.js
@@ -3,16 +3,15 @@ import OpenServer from './src/Server.js'
 import CreateCluster from './src/Cluster.js'
 
 const PORT = process.env.PORT || 8080
-puerto = PORT
 
 switch(modo){
     case 'f':
     case 'fork':
-        OpenServer(puerto)
+        OpenServer(PORT)
         break
     case 'c':
     case 'cluster':
-        CreateCluster(puerto)
+        CreateCluster(PORT)
         break
     default:
         const error = new Error("Parametro MODO pasado con parametro incorrecto. Usar fork o cluster")
