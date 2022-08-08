8c3309535fe56f37a14a4d4e128858d426f2e79c
jamuzio <jamuzio@gmail.com>
2022-08-08 18:48:34 -0300

    Add PORT Env variable

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import { puerto, modo}  from './Config/Yargs_config.js'
 import OpenServer from './src/Server.js'
 import CreateCluster from './src/Cluster.js'
 
+const PORT = process.env.PORT || 8080
+puerto = PORT
+
 switch(modo){
     case 'f':
     case 'fork':
